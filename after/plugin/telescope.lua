f3e569d8dec3f5e80ece2569f36f84de05e18a10
Dan Lowe <dan@tangledhelix.com>
2023-06-16 08:31:51 -0400

    Add <leader>fw: find cursor word using telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,4 +4,5 @@ vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find Git file(s)"
 vim.keymap.set("n", "<leader>fs", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = "Find String" })
+vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Find Word" })
 vim.keymap.set("n", "<leader>fa", builtin.resume, { desc = "Find Again (open picker in previous state)" })
