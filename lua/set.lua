4e60362a6964fc1a9e16dfe4609972495a3d0cd4
Dan Lowe <dan.lowe@global.ntt>
2023-09-12 21:01:31 -0400

    Move vimwiki options to set.lua

--- lua/set.lua
+++ lua/set.lua
@@ -58,3 +58,15 @@ opt.swapfile = false
 opt.backup = false
 opt.undofile = true
 
+-- vimwiki options must occur before plugins are loaded
+
+-- Limit vimwiki to its own directory; .md files outside of the vimwiki
+-- directory are just normal Markdown files
+vim.g.vimwiki_global_ext = 0
+
+-- Set up vimwiki. This has come before the plugin is loaded; easiest to
+-- ensure that here.
+vim.cmd([[
+let g:vimwiki_list = [{'path': '~/vimwiki/', 'syntax': 'markdown', 'ext': '.md'}]
+]])
+
