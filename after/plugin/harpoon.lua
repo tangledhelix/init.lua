3110fae1ba73d382417d6193150531ed8c3a0108
Dan Lowe <dan@tangledhelix.com>
2023-09-25 22:46:18 -0400

    Map C-j, C-k to harpoon next/prev jump

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -9,3 +9,6 @@ vim.keymap.set("n", "<leader><leader>", ui.toggle_quick_menu)
 -- vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
 -- vim.keymap.set("n", "<C-;>", function() ui.nav_file(4) end)
 
+-- Ctrl-[j,k] to nav through the list
+vim.keymap.set("n", "<C-j>", function() ui.nav_next() end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_prev() end)
