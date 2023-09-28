215d4eae4def904be207de0b4ad5a7f9b33fd36c
Dan Lowe <dan.lowe@global.ntt>
2023-09-28 14:14:27 -0400

    moar telescope window size!

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,7 +3,7 @@
 require("telescope").setup{
     defaults = {
         layout_strategy = 'vertical',
-        layout_config = { height = 0.95 },
+        layout_config = { height = 0.95, width = 0.95 },
     }
 }
 
