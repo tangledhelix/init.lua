98908623c63cd908cdc5ea23df22ecf6dddbaac2
Dan Lowe <dan@tangledhelix.com>
2023-12-15 22:19:25 -0500

    whitespace cleanup

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -35,7 +35,7 @@ autocmd({ "BufWritePre" }, {
 autocmd({ "FileType" }, {
 	pattern = "qf",
 	callback = function()
-                vim.opt_local.number = true
-                vim.opt_local.relativenumber = false
+    vim.opt_local.number = true
+    vim.opt_local.relativenumber = false
 	end,
 })
