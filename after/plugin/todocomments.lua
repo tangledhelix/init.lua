c180af23ca4e29a1901bc684f9cc7f2e4a4dc6b0
Dan Lowe <dan@tangledhelix.com>
2023-12-15 22:38:46 -0500

    whitespace cleanup

--- after/plugin/todocomments.lua
+++ after/plugin/todocomments.lua
@@ -2,9 +2,10 @@
 require("todo-comments").setup({})
 
 vim.keymap.set("n", "]t", function()
-	require("todo-comments").jump_next()
+  require("todo-comments").jump_next()
 end, { desc = "Next todo comment" })
 
 vim.keymap.set("n", "[t", function()
-	require("todo-comments").jump_prev()
+  require("todo-comments").jump_prev()
 end, { desc = "Previous todo comment" })
+
